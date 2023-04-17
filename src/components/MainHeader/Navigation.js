104a75f75b7440294c56ccb21a06cb90af80000d
Pankaj10101 <rkay1048@gmail.com>
2023-04-17 17:05:03 +0530

    using useContext api

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,24 +1,25 @@
-import React from 'react';
-
+import React, {useContext} from 'react';
+import AuthContext from '../../context/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  const csx = useContext(AuthContext)
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {csx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {csx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {csx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={csx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
